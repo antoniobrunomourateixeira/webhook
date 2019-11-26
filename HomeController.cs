b7b8b4e6858c5634e668abd5783db4cc540c894f
antoniobrunomourateixeira <antoniobrunobm@gmail.com>
2019-11-26 11:06:12 -0300

    commit 3

---
+++ HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebHooks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Webhook
+{
+    public class HomeController : Controller
+    {
+        [GitHubWebHook]
+        public IActionResult GitHubHandler(JObject data)
+        {
+            var result = JsonConvert.SerializeObject(data);
+            return Ok();
+        }
+    }
+}
